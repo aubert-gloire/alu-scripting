62538ee2646bf1f06c09b86782af325be883d97c
aubert-gloire <156434542+aubert-gloire@users.noreply.github.com>
2024-07-06 13:24:27 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python3
-"""Return the number of subscribers of a given subreddit"""
+
+"""
+This module contains a function that queries the Reddit API and
+returns the number of subscribers for a given subreddit.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that fetches number_of_subscribers"""
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    """
+    Fetches the number of subscribers for a given subreddit.
 
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers or 0 if an error occurs.
+    """
+    USER_AGENT = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+    URL = f"https://api.reddit.com/r/{subreddit}/about"
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        return RESPONSE.json().get("data").get("subscribers")
-
+        response = requests.get(URL, allow_redirects=False,
+                                headers={'User-Agent': USER_AGENT}).json()
+        return response.get("data", {}).get("subscribers", 0)
     except Exception:
         return 0
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
