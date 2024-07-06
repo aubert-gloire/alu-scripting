8ec7873c46551a235d8ad55f5ceaec9e464c87b3
aubert-gloire <a.bihibindi1@alustudent.com>
2024-07-06 11:15:37 +0000

    error handling

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,37 +1,17 @@
 #!/usr/bin/python3
-
-"""
-A module to fetch and return the number of subscribers for a given subreddit.
-"""
+"""Return the number of subscribers of a given subreddit"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Fetches the number of subscribers for a given subreddit.
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
 
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers or 0 if an error occurs.
-    """
-    USER_AGENT = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
-    URL = f"https://api.reddit.com/r/{subreddit}/about"
     try:
-        response = requests.get(URL, allow_redirects=False,
-                                headers={'User-Agent': USER_AGENT}).json()
-        return response.get("data", {}).get("subscribers", 0)
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
     except Exception:
         return 0
-
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
