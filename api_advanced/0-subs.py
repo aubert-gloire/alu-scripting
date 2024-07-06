db3d397f2829c9422092e77a97355c4d2d858037
aubert-gloire <156434542+aubert-gloire@users.noreply.github.com>
2024-07-06 13:35:42 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-# get subs
-from requests import get
-from sys import argv
+"""
+function that queries the 'Reddit API' and returns the number of subscribers
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """subs"""
-    head = {'User-Agent': 'Dan Kazam'}
-    count = get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit), headers=head).json()
-    try:
-        return count.get('data').get('subscribers')
-    except:
-        return 0
+    """
+    number of subscribers
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
 
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
